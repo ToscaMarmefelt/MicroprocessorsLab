8cd8e0cedd9086295abff2eb6f32b18d324dd83c
thc16 <thc16@skbl-422-008.ph.ic.ac.uk>
2018-10-15 16:35:38 +0100

    Make OP, D0 and OE change at different times. For external 1 byte memory.

--- Simple1.asm
+++ Simple1.asm
@@ -7,21 +7,35 @@
 	org 0x100		    ; Main code starts here at address 0x100
 
 start
-	movlw   0xFF
-	movwf	TRISD, ACCESS	    ; Port D all inputs
+	movlw   0x00
+	movwf	TRISD, ACCESS	    ; Port D all outputs
 	movlw 	0x0
 	movwf	TRISE, ACCESS	    ; Port E all outputs
+	
+oe	;movlw	0xF0
+	;movwf	PORTE, ACCESS	    ; Data to be stored on external memory 
 	
 	movlw	0xFF
 	movwf	0x06		    ; Create counter at 0x06 
 	
-clock	movlw	0x02		    ; Clock (pin 0) 0 ; OE (pin 1) 1		    
+clock	movlw	0xF0
+	movwf	PORTE, ACCESS	    ; Data to Port E 
+	
+	movlw	0x00		    ; CP (pin 0) 0 ; OE (pin 1) 0		    
 	movwf	PORTD, ACCESS	    ; Output clock value 0 to Port D
-	movlw	0x03		    ; Clock (pin 0) 1 ; OE (pin 1) 1
-	movwf	PORTD, ACCESS	    ; Output clock value 1 to Port D (OE still 1) 
+	movlw	0x03		    ; CP (pin 0) 1 ; OE (pin 1) 1
+	movwf	PORTD, ACCESS	    ; Output clock value 1 to Port D 
+	movlw	0xF1
+	movwf	PORTE, ACCESS	    ; Data to Port E
+	movlw	0x01		    ; CP (pin 0) 1 ; OE (pin 1) 0
+	movwf	PORTD, ACCESS
+	movlw	0x02		    ; CP (pin 0) 0 ; OE (pin 1) 1
+	movwf	PORTD, ACCESS
+	movlw	0x03		    ; CP (pin 0) 1 ; OE (pin 1) 1
+	movWf	PORTD, ACCESS	    
 	decfsz	0x06, F, ACCESS	    ; Count
 	bra	clock
 	
-	goto	start
+	goto	0x0
 	
 	end

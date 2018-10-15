c3013517723cf5f9541ebd38e341ececd7c8914f
thc16 <thc16@skbl-422-008.ph.ic.ac.uk>
2018-10-15 10:35:00 +0100

    Input from Port D and delayed loop time

--- Simple1.asm
+++ Simple1.asm
@@ -7,15 +7,31 @@
 	org 0x100		    ; Main code starts here at address 0x100
 
 start
+	movlw   0xFF
+	movwf	TRISD, ACCESS	    ; Port D all inputs
 	movlw 	0x0
-	movwf	TRISB, ACCESS	    ; Port C all outputs
+	movwf	TRISC, ACCESS	    ; Port C all outputs
 	bra 	test
-loop	movff 	0x06, PORTB
+loop	movlw	0xFF		    ; select how long the delay will be
+	movwf	0x20		    ; Make 0x20 counter
+	call	delay
+	movff 	0x06, PORTC
 	incf 	0x06, W, ACCESS
 test	movwf	0x06, ACCESS	    ; Test for end of loop condition
-	movlw 	0x63
+	movlw	0xFF		    ; Set number of counts of main counter
 	cpfsgt 	0x06, ACCESS
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
+	
+delay	movlw	0xFF		    ; Select number of counts for second delay
+	movwf	0x21		    ; Second counter address
+	call	delay2
+	decfsz	0x20		    ; Decrement value in counter until zero
+	bra	delay
+	return
+	
+delay2	decfsz	0x21		    ; Decrement value in second counter until zero
+	bra	delay2		    
+	return
 	
 	end

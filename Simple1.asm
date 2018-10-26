10847a521c5e91bb8d62c918ba31304c57df7c13
thc16 <thc16@skbl-422-008.ph.ic.ac.uk>
2018-10-26 16:49:08 +0100

    Added delay subroutine and implemented it in CP_1 and CP_2

--- Simple1.asm
+++ Simple1.asm
@@ -1,11 +1,13 @@
 	#include p18f87k22.inc
 	
+	delay_count	res 1		    ; Location for delay counter
+	
 	code
 	org 0x0
 	goto	start
 	
 	org 0x100		    ; Main code starts here at address 0x100
-
+	
 	; First 4 lines I have no clue what they are on about... 
 	banksel	PADCFG1		    ; PADCFG1 is not in Access Bank
 	bsf	PADCFG1, REPU, BANKED ; PORT E pull-ups on
@@ -36,7 +38,7 @@ read	movlw	0xFF
 
 	movlw	0x20		    ; 0010 0000
 	movwf	PORTD, ACCESS	    ; Memory 1 output enabled. OE1* = 0 and OE2* = 1. 
-	call	CP_1		    ; DO WE NEED THIS????   
+	call	CP_1		    ; DO WE NEED THIS????   Won't do any harm to have it there at least?
 	movff	PORTE, PORTC	    ; Read PORT E to PORT C (LEDs)
 
 	movlw	0x02		    ; 0000 0010
@@ -47,21 +49,32 @@ read	movlw	0xFF
 	movlw	0x22
 	movwf	PORTD, ACCESS	    ; Disable both memory outputs
 	
-delay		
 	
+	; Use this delay subroutine if needed
+delay	movlw	0x64		    ; Count to .100
+	movwf	delay_count
+loop	decfsz	delay_count	    ; Decrement until 0
+	bra	loop
+	return
 	
+	; Let CP1 = 1 then 0
 CP_1	movff	PORTD, W	    ; Put value of PORT D in W	    
 	iorlw	0x01		    
 	movwf	PORTD, ACCESS	    ; CP1 = 1. Original PORT D settings on <7:1>.
+	call	delay
 	xorlw	0x01		    
 	movwf	PORTD, ACCESS	    ; CP1 = 0. Original PORT D settings on <7:1>.
+	call	delay
 	return
 	
+	; Let CP2 = 1 then 0
 CP_2	movff	PORTD, W	    ; Put value of PORT D in W	    
 	iorlw	0x10		    
 	movwf	PORTD, ACCESS	    ; CP2 = 1. Original PORT D settings on all other pins.
+	call	delay
 	xorlw	0x10		    
 	movwf	PORTD, ACCESS	    ; CP2 = 0. Original PORT D settings on all other pins. 
+	call	delay
 	return
 
 	goto	0x00

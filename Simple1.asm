a653532337e9e85af65b17d7e281423223a76062
Tosca <Tosca@dyn3185-24.wlan.ic.ac.uk>
2018-10-27 16:33:36 +0100

    Implemented read/write_1/2 functions

--- Simple1.asm
+++ Simple1.asm
@@ -1,6 +1,7 @@
 	#include p18f87k22.inc
 	
-	delay_count	res 1		    ; Location for delay counter
+	delay_count	res 1	    ; Location for delay counter
+	data_temp	res 1	    ; Reserve 1 byte in memory for temporary storage of data
 	
 	code
 	org 0x0
@@ -14,6 +15,8 @@
 	movlb	0x00		    ; Set BSR back to Bank 0
 	setf	TRISE		    ; Tri-state PORT E
 	
+	
+	; Set up static ports (PORT D, C, H)
 start	movlw	0x00
 	movwf	TRISD, ACCESS	    ; PORT D (Address/Control bus) all outputs
 	
@@ -21,37 +24,70 @@ start	movlw	0x00
 	movwf	TRISC, ACCESS	    ; PORT C (Memory 1 LEDs) all inputs
 	movwf	TRISH, ACCESS	    ; PORT H (Memory 2 LEDs) all outputs
 	
-write	movlw	0x22		    
-	movwf	PORTD, ACCESS	    ; Disable both memory outputs
-	movlw	0x00	
-	movwf	TRISE, ACCESS	    ; PORT E (Data bus) all outputs
-	
+	; Write/read test routine
 	movlw	0x6F		    ; Set 1st data pattern: b'1001 1111'
-	movwf	PORTE, ACCESS	    ; Output data to PORT E
-	call	CP_1		    ; CP1 = 1 then 0. Rest of PORT D remain unchanged. 
+	call	write_1		    ; Write data pattern onto Memory 1
 	movlw	0xDB		    ; Set 2nd data patterm: b'1101 1011'
-	movwf	PORTE, ACCESS	    ; Output data to PORT E
-	call	CP_2		    ; CP2 = 1 then 0. Rest of PORT D remain unchanged. 
+	call	write_2		    ; Write 2nd data pattern onto Memory 2
 	
-read	movlw	0xFF
-	movwf	TRISE, ACCESS	    ; PORT E (Data bus) all inputs
+	call	read_1		    ; Read Memory 1 (will be displayed on PORT C LEDs)
+	call	read_2		    ; Read Memory 2 (displayed on PORT H LEDs)
+	
+	
+	goto	0x00
+	
+	
+	; SUBROUTINES
+	
+	; Write data in W onto Memory 1
+write_1	movwf	data_temp	    ; Temporarily store data value in reserved location
+	movlw	0x22		    
+	movwf	PORTD, ACCESS	    ; Disable both memory outputs
+	clrf	TRISE, ACCESS	    ; PORT E (Data bus) all outputs
+	
+	movff	data_temp, PORTE    ; Output data from temporary storage to PORT E
+	call	CP_1		    ; CP1 = 1 then 0. Rest of PORT D remain unchanged. 
+	return
 
+	; Write data in W onto Memory 2
+write_2	movwf	data_temp	    ; Temporarily store data value in reserved location
+	movlw	0x22		    
+	movwf	PORTD, ACCESS	    ; Disable both memory outputs
+	clrf	TRISE, ACCESS	    ; PORT E (Data bus) all outputs
+	
+	movff	data_temp, PORTE    ; Output data from temporary storage to PORT E
+	call	CP_2		    ; CP2 = 1 then 0. Rest of PORT D remain unchanged. 
+	return
+	
+	; Read data from Memory 1 onto W
+read_1	setf	TRISE, ACCESS	    ; PORT E (Data bus) all inputs
 	movlw	0x20		    ; 0010 0000
 	movwf	PORTD, ACCESS	    ; Memory 1 output enabled. OE1* = 0 and OE2* = 1. 
-	call	CP_1		    ; DO WE NEED THIS????   Won't do any harm to have it there at least?
-	movff	PORTE, PORTC	    ; Read PORT E to PORT C (LEDs)
-
+	call	CP_1		    ; DO WE NEED THIS to make change in OE happen????   Won't do any harm to have it there at least?
+	
+	movff	PORTE, data_temp    ; Read data from PORT E to data_temp
+	movff	data_temp, PORTC    ; Display data on PORT C LEDs
+	movlw	0x22		    ; 0010 0010
+	movwf	PORTD, ACCESS	    ; Disable both memory outputs. OE1* = 1 and OE2* = 1.
+	movff	data_temp, W	    ; Leave subroutine with data stored in W
+	return
+	
+	; Read data from Memory 2 onto W
+read_2	setf	TRISE, ACCESS	    ; PORT E (Data bus) all inputs
 	movlw	0x02		    ; 0000 0010
 	movwf	PORTD, ACCESS	    ; Memory 2 output enabled. OE1* = 1 and OE2* = 0.
 	call	CP_2		    ; DO WE NEED THIS????   
-	movff	PORTE, PORTH	    ; Read PORT D to PORT H (LEDs)
-
-	movlw	0x22
-	movwf	PORTD, ACCESS	    ; Disable both memory outputs
 	
+	movff	PORTE, data_temp    ; Read data from PORT D to data_temp
+	movff	data_temp, PORTH    ; Display data on PORT H LEDs
+	movlw	0x22		    ; 0010 0010
+	movwf	PORTD, ACCESS	    ; Disable both memory outputs. OE1* = 1 and OE2* = 1.
+	movff	data_temp, W	    ; Leave subroutine with data stored in W
+	return
 	
-	; Use this delay subroutine if needed
-delay	movlw	0x64		    ; Count to .100
+	; Delay subroutine which takes 250 ns to run NOT DONE YET
+delay250ns	
+	movlw	0x64		    ; Count to .100
 	movwf	delay_count
 loop	decfsz	delay_count	    ; Decrement until 0
 	bra	loop
@@ -61,7 +97,7 @@ loop	decfsz	delay_count	    ; Decrement until 0
 CP_1	movff	PORTD, W	    ; Put value of PORT D in W	    
 	iorlw	0x01		    
 	movwf	PORTD, ACCESS	    ; CP1 = 1. Original PORT D settings on <7:1>.
-	call	delay
+	call	delay250ns
 	xorlw	0x01		    
 	movwf	PORTD, ACCESS	    ; CP1 = 0. Original PORT D settings on <7:1>.
 	call	delay
@@ -71,7 +107,7 @@ CP_1	movff	PORTD, W	    ; Put value of PORT D in W
 CP_2	movff	PORTD, W	    ; Put value of PORT D in W	    
 	iorlw	0x10		    
 	movwf	PORTD, ACCESS	    ; CP2 = 1. Original PORT D settings on all other pins.
-	call	delay
+	call	delay250ns
 	xorlw	0x10		    
 	movwf	PORTD, ACCESS	    ; CP2 = 0. Original PORT D settings on all other pins. 
 	call	delay

a2d1de040e8036570add55b1c71c9a6dd194f218
thc16 <thc16@skbl-422-008.ph.ic.ac.uk>
2018-10-29 16:04:39 +0000

    Added 8by24-bit multiplication. Working!

--- Simple1.asm
+++ Simple1.asm
@@ -25,7 +25,12 @@ prod0	res 1	    ; In total product (prod0:3) will require 4 bytes (32 bits)
 prod1	res 1
 prod2	res 1
 prod3	res 1
-
+	
+	; ******* Reserve data space for 8-bit by 24-bit multiplication
+l_24	res 1   ; 1 byte for each of the low, middle and upper bytes of 24-bit number
+m_24	res 1
+u_24	res 1
+  
 ;tables	udata	0x400    ; reserve data anywhere in RAM (here at 0x400)
 ;myArray res 0x80    ; reserve 128 bytes for message data
 
@@ -60,19 +65,32 @@ test816	movlw	0x1F
 	
 	; ******* Code to check 16x16-bit numtiplication
 test1616
-	movlw	0xF8		; First 16-bit number: 0xF81D
-	movwf	sixteen_u	; upper byte
-	movlw	0x1D
-	movwf	sixteen_l	; lower byte
-	movlw	0xBB		; Second 16-bit number: 0xBB92
-	movwf	sixteen2_u	; upper byte
-	movlw	0x92
-	movwf	sixteen2_l	; lower byte
+	;movlw	0xF8		; First 16-bit number: 0xF81D
+	;movwf	sixteen_u	; upper byte
+	;movlw	0x1D
+	;movwf	sixteen_l	; lower byte
+	;movlw	0xBB		; Second 16-bit number: 0xBB92
+	;movwf	sixteen2_u	; upper byte
+	;movlw	0x92
+	;movwf	sixteen2_l	; lower byte
 	
-	
-	call	mul16by16	; Multiply 16-bit number by 16-bit number
+
+	;call	mul16by16	; Multiply 16-bit number by 16-bit number
 				; Result stored in prod3:0
-		
+	
+	
+	; ******* Code to check 8x24-bit multiplication
+test824	movlw	0x74		
+	movwf	eight		; Set value for 8-bit number
+	movlw	0x0A
+	movwf	l_24	; Value for lower byte of 24-bit number
+	movlw	0xF2
+	movwf	m_24	; Value for mid byte of 24-bit number	
+	movlw	0xEF
+	movwf	u_24		; Value for upper byte of 24-bit number
+	
+	call	mul8by24
+			
 				
 	; 8-bit by 16-bit multiplication
 mul8by16    
@@ -92,6 +110,31 @@ mul8by16
 	movf	PRODH, W	; Move upper byte of second product to W	
 	addwfc	res2, F		; Add W, res2 (=empty) and carry bit. Place in res2. 
 	return
+	
+	; 8-bit by 24-bit multiplication
+mul8by24
+	movf	l_24, W		; Move lower byte to W
+	mulwf	eight		; Multiply 8-bit number by lower byte of 24-bit number
+	movff	PRODL, prod0	; Final result of lowest byte of product
+	movff	PRODH, prod1	; Temporary storage
+	
+	
+	movf	m_24, W		; Move mid byte to W
+	mulwf	eight		; Multiply 8-bit number by mid byte of 24-bit number
+	movff	PRODH, prod2	; Temporary storage
+	movf	PRODL, W
+	addwf	prod1, F	; Second byte of first multiplication + first byte of second multiplication. Might give carry!
+	
+	movf	u_24, W		; Move upper byte to W
+	mulwf	eight		; Multiply 8-bit number by upper byte of 24-bit number
+	movf	PRODL, W
+	addwfc	prod2, F	; Second byte of second multiplication + first byte of third multiplication. Might give carry!
+	movlw	0x00
+	movwf	prod3		; Make sure prod3 is empty beforehand
+	movf	PRODH, W
+	addwfc	prod3		; Second byte of third multiplication + carry
+	return
+	
 	
 	; 16-bit by 16-bit multiplication
 mul16by16   
